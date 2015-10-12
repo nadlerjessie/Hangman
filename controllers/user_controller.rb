6538fdfe178c643cd12efca1de99e2210f4f309a
chenghw <ho.won.cheng@gmail.com>
2015-10-11 21:50:41 -0400

    edited the controller file to become the run file and created a user and board controller

---
+++ controllers/user_controller.rb
@@ -0,0 +1,90 @@
+require_relative '../lib/concerns/graphic.rb'
+
+class UserController
+
+  include ::Graphic
+
+  def create_user
+    puts "Please input your name."
+    current_user = User.new(gets.chomp)
+  end
+
+  def list_users
+    User.all.each_with_index do |user, index|
+      puts "#{index+1} - #{user.name}"
+    end
+  end
+
+  def switch_users
+    list_users
+    begin
+      puts "Please select user number."
+      user_switch = gets.chomp.to_i-1
+      switched_user = User.all[user_switch]
+    end while user_switch >= User.all.count
+    switched_user
+  end
+
+  def display_leaderboard
+    puts "\n*** LEADERBOARD ***"
+    User.leaderboard.each do |rankings|
+      puts "#{rankings.name}: #{rankings.wins}-#{rankings.losses}"
+    end
+  end
+
+  def play(user)
+    begin
+      board = Board.new
+      user.boards << board
+      user.reset_lives
+      puts "\nHi #{user.name}! You start with #{user.lives} lives."
+      begin
+        fishtank(user)
+        puts "\n\n\n #{board.reveal_letter.split('').join(' ')}"
+        puts "Incorrect Letters Guessed:\n#{board.incorrect.sort.join(' ')}"
+        
+        if user.lives != 1
+          puts "You have #{user.lives} lives left."
+        else
+          puts "You have #{user.lives} life left."
+        end
+        
+        puts "Please make a guess."
+        current_guess = user.make_guess.upcase
+        
+        if board.check_valid?(current_guess)
+          if board.check_guess?(current_guess)
+            board.reveal_letter
+            board.store_correct
+          else
+            board.store_incorrect
+            user.lives -= 1
+          end
+        else
+          puts "That's not a valid entry."
+        end
+      end while user.lives > 0 && board.secret_word != board.revealed_letters
+    end while game_over(user, board) == "yes"
+  end
+
+  def win?(board)
+    board.secret_word == board.revealed_letters
+  end
+
+  def game_over(user, board)
+    if win?(board)
+      user.update_games_played("win")
+      puts board.reveal_letter
+      puts "You win! Do you want to play again?"
+    else
+      user.update_games_played("lose")
+      fishtank(user)
+      puts "You lose! The word was: #{board.secret_word}.\nDo you want to play again?"
+    end
+    begin
+      puts "Please enter yes or no:"
+      y_n = gets.chomp.downcase
+    end while !(y_n == "yes" || y_n == "no")
+    y_n
+  end
+end
