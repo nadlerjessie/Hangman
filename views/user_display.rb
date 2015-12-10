e2715006f9b0b100120e590533afa2552aee34b0
chenghw <ho.won.cheng@gmail.com>
2015-12-10 09:22:40 -0500

    ready to merge

---
+++ views/user_display.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/concerns/graphic.rb'
+
+class UserDisplay
+  include ::Graphic
+
+  def render(user)
+    fishtank(user)
+    puts "\n\n\n #{user.boards[-1].reveal_letter.split('').join(' ')}"
+    puts "Incorrect Letters Guessed:\n#{user.boards[-1].incorrect.sort.join(' ')}"
+  end
+end
---
+++ views/user_lose.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/concerns/graphic.rb'
+
+class UserLose
+  include ::Graphic
+
+  def render(user)
+    fishtank(user)
+    puts "You lose! The word was: #{user.boards[-1].secret_word}.\nDo you want to play again?"
+  end
+end
